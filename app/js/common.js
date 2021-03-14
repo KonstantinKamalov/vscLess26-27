7cbaa712a3bd8e19b84c7bd219b8dad1fd3402b1
KamalovKonstantin <kamalov.konstantin@gmail.com>
2021-03-14 23:32:41 +0700

    athome: доверстать слайдер

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,6 @@
-$(function() {
-
-	// Custom JS
-
+$('.single-items').slick({
+	arrows: true,
+	dots: true,
+	prevArrow: <div class="slider-arrow slider-arrow__left"></div>,
+	nextArrow: <div class="slider-arrow slider-arrow__right"></div>
 });
